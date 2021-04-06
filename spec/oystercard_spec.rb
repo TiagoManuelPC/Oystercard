53e33c62d405d869c05f04404e0cc0ec3142b5d2
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-04-06 15:28:36 +0100

    challenge 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,11 @@
 require 'oystercard'
 describe Oystercard do
 
-it 'has a balance' do
-    expect(subject.balance).to eq(0)   
-end
+    it 'has a balance' do
+        expect(subject.balance).to eq(0)   
+    end
+
+    it 'can be top up' do
+        expect(subject.top_up(10)).to eq(10)
+    end
 end
